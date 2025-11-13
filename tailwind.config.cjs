c6423de24f4e0389b6f575065801e46660f16b2c
Pavan <pavanagnihotri82@gmail.com>
2025-11-13 18:12:07 +0530

    fix: tailwind config js for render build

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+﻿/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
